c1e073cd371402e06f4c91869520e276743e6702
NoOneISeQual <kurdefrance1613@gmail.com>
2023-05-12 19:55:05 +0300

    Add condition

--- Program.cs
+++ Program.cs
@@ -6,3 +6,16 @@ string[] StringArray2 = new string [n];
 
 int max = 3;
 int count = 0;
+
+for (int i = 0; i < StringArray.Length; i++)
+{
+    Console.WriteLine($"Введите {i+1} элемент массива:");
+    StringArray[i] = Convert.ToString(Console.ReadLine()!);
+    {
+        if (StringArray[i].Length <= max)
+        {
+            StringArray2[count] = StringArray[i];
+            count++;
+        }
+    }
+}
