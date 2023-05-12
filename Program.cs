e238d128bb902697364cc9c73e8f2ce1af8a5f23
NoOneISeQual <kurdefrance1613@gmail.com>
2023-05-12 19:53:54 +0300

    Add new arrays

--- Program.cs
+++ Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+string[] StringArray = new string [n];
+string[] StringArray2 = new string [n];
